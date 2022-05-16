401ce394486306deee2ae319d6f920cbb28ec252
Rich23 <97440488+Richmund23@users.noreply.github.com>
2022-05-16 10:25:56 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // ========== STICKY NAVHEADER ON SCROLL ==========
+        if(this.scrollY > 20){
+            $('.navheader').addClass("sticky");
+        }else{
+            $('.navheader').removeClass("sticky");
+        }
+        
+        // ========== SCROLL UP BUTTON HIDE/SHOW ==========       
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // ========== SLIDE UP SCRIPT ==========
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navheader .links li a').click(function(){
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // ========== ACTIVE NAVLINK ==========
+    // $('.navheader .links li a').click(function(){
+    //     $('.links li a').removeClass("active");
+    //     $(this).addClass("active");
+    // });
+
+    // ========== TOGGE LINKS/NAVHEADER ==========
+    $('.links-btn').click(function(){
+        $('.navheader .links').toggleClass("active");
+        $('.links-btn i').toggleClass("active");
+    });
+
+    // ========== TYPING ANIMATION ==========
+    var typed = new Typed(".typing", {
+        // strings: [" " , "I LOVE YOU", "STAY STRONG",
+        // "I LOVE YOU", "THERE'S MORE TO COME"],
+        strings: [" " , "am an aspiring Associate Software Engineer", "am an Instruction Specialist",
+        "was a former Intern Technical Staff", "was a former Intern IT Support "],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: [" ", "Richmund Garces Allorde"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // ========== OWL CAROUSEL ==========
+    $('.carousel').owlCarousel({
+        margin: 30,
+        // autoWidth: true,
+        // items: 1,
+        // center: true,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 20,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
