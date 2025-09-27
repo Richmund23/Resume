ccb34a88e6179d919a8249566e906645f533e1a7
Rich23 <97440488+Richmund23@users.noreply.github.com>
2025-09-27 16:32:13 +0800

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
 
     // ========== TYPING ANIMATION ==========
     var typed = new Typed(".typing", {
-        strings: [" " , "am a Business Intelligence Developer", "was a former Data Management Analyst", "was a former Instructor",
+        strings: [" " , "am a Business Intelligence Developer", "was a former Data Management Analyst", "was a former Instruction Specialist",
         "was a former Intern Technical Staff", "was a former Intern IT Support "],
         typeSpeed: 100,
         backSpeed: 60,
